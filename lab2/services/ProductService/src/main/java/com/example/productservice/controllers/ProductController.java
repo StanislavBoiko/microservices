a7c8b916998d53dc4791a7f9e7241cbad227f16a
Stanislav Boiko <stanislavboiko33@gmail.com>
2023-05-30 14:08:05 +0300

    hotfix

--- lab2/services/ProductService/src/main/java/com/example/productservice/controllers/ProductController.java
+++ lab2/services/ProductService/src/main/java/com/example/productservice/controllers/ProductController.java
@@ -3,7 +3,7 @@ package com.example.productservice.controllers;
 import com.example.productservice.entities.Product;
 import com.example.productservice.services.ProductService;
 import com.example.productservice.services.ProductServiceImpl;
-import org.json.JSONObject;
+import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +18,8 @@ public class ProductController {
     @PostMapping("product")
     public ResponseEntity createProduct(@RequestBody JSONObject jsonReq){
         var product = new Product();
-        product.setName(jsonReq.getString("Name"));
-        product.setPrice(jsonReq.getFloat("Price"));
+        product.setName(jsonReq.getAsString("Name"));
+        product.setPrice((long)jsonReq.getAsNumber("Price"));
         productService.addProduct(product);
         return ResponseEntity.ok().build();
     }
